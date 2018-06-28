df187bd992f5f5ac4c27444541df17d079492cf8
kurryderp <kurrytran32@gmail.com>
2018-06-27 17:14:09 -0700

    stuck on timer calc

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TrainToNarnia
+
+##
--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,12 +1,11 @@
-
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyDnhFRMt1ERHgbyAoo2zHC5VrTH5rl5TDM",
-    authDomain: "timesheet-15e9e.firebaseapp.com",
-    databaseURL: "https://timesheet-15e9e.firebaseio.com",
-    projectId: "timesheet-15e9e",
-    storageBucket: "timesheet-15e9e.appspot.com",
-    messagingSenderId: "404480687023"
+    apiKey: "AIzaSyCfPa4DIYUa73gQ9XfoqO-UUVjbEd3S6n8",
+    authDomain: "traintonarnia-84e14.firebaseapp.com",
+    databaseURL: "https://traintonarnia-84e14.firebaseio.com",
+    projectId: "traintonarnia-84e14",
+    storageBucket: "traintonarnia-84e14.appspot.com",
+    messagingSenderId: "1037847930921"
   };
   firebase.initializeApp(config);
 
@@ -14,18 +13,23 @@
 
   $('button:submit').on('click', function(){
       event.preventDefault();
-      let name = $('#emName').val().trim();
-      let start = moment($('#start').val().trim(), 'DD-MM-YY').format('X');
-      let position = $('#position').val().trim();
-      let rate = $('#rate').val().trim();
+      let trainName = $('#trainName').val().trim();
+      //parsing train time to military time format("X") changes into unix
+      let firstTrain = moment($('#firstTrain').val().trim(), 'HH:mm');
+      let destination = $('#destination').val().trim();
+      //parsing frequency into minutes
+      let freq = $('#freq').val().trim();
 
-      database.ref().push({
-        name: name,
-        start: start,
-        position: position,
-        rate: rate,
+      //setting new values into a variable for easier firebase push
+      let fireVal = {
+        fireName: trainName,
+        fireStart: firstTrain,
+        fireDestination: destination,
+        fireFreq: freq,
         dateAdded: firebase.database.ServerValue.TIMESTAMP
-      })
+      };
+
+      database.ref().push(fireVal);
       console.log(2);
 
   });
@@ -36,32 +40,25 @@
     
     
     //sets local values to firebase values
-    empName = snap.name;
-    empStart = snap.start;
-    empPosition = snap.position;
-    empRate= snap.rate;
-    console.log(empName);
-    console.log(empStart);
+    tName = snap.fireName;
+    tStart = snap.fireStart;
+    tDestination = snap.fireDestination;
+    tFreq= snap.fireFreq;
+    console.log(tName);
+    console.log(tDestination);
+    console.log(tStart)
 
-    //calculating amount billed based on months worked 
-    let timeFormat = "DD/MM/YY"
-    let startDate = moment(empStart, timeFormat);
-    let today = moment().format(timeFormat);
-    let timePassed= moment().diff(moment(empStart,'X'), 'months' );
-
+    //next train = (difference of time) %
+    let nowUnix = moment().unix();
+    console.log(nowUnix)
     
-    // 
-    console.log(timePassed);
-    // console.log(secondsPassed + 'seconds')
     
-
-    //added items into table body
     
-    //defining variables for table row and table head
+    // defining variables for table row and table head
     let $tr = $('<tr>');
     let $td = '<td>';
     
-    $tr.append($td + empName + $td + empPosition + $td + empStart + $td + "NaN" + $td + empRate + $td + "NaN");
+    $tr.append($td + tName + $td + tDestination + $td + tFreq + $td + "NaN" + $td + "NaN");
 
     $('tbody').append($tr);
 
